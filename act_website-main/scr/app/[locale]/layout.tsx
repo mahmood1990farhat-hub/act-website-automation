8b342a716b930b8523321b21299627036a7c6524
hfonlinecreation-main <mahmood1990farhat@hotmail.com>
2026-05-28 18:25:50 +0100

    Add Google Search Console verification tag
    Refactor RootLayout to include HTML structure and meta tags.

--- act_website-main/scr/app/[locale]/layout.tsx
+++ act_website-main/scr/app/[locale]/layout.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
+
 export const metadata = {
-  title: "ACT",
-  description: "Airport & City Group",
+  title: 'ACT',
+  description: 'Airport & City Group',
 };
 
 export default function RootLayout({
@@ -8,5 +10,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="-JqU021-FySgKIvmSaumQHIfWj7G5DqRJurNdON7xOc"
+        />
+      </head>
+
+      <body>{children}</body>
+    </html>
+  );
 }
